32ccdff5a13832259024ed7c21814b9aa5b9ba79
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-11-11 23:59:40 +0500

    8

---
+++ Tyuiu.BayazitovEA.Sprint4.Task7.V11.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.BayazitovEA.Sprint4.Task7.V11.Lib
+{
+    public class DataService : ISprint4Task7V11
+    {
+        public int Calculate(int n, int m, string value)
+        {
+            int[,] mtrx = new int[n, m];
+            int index = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    mtrx[i, j] = int.Parse(value.Substring(i * m + j, 1));
+                }
+            }
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (mtrx[i, j]%2 == 1) { count++; }
+
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.BayazitovEA.Sprint4.Task7.V11/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BayazitovEA.Sprint4.Task7.V11.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = 4;
+        int m = 2;
+        int[,]mtrx=new int[n,m];
+        string str = "56789012";
+        DataService ds = new DataService(); ;
+        int index=0 ;
+
+        int res = ds.Calculate(n, m, str);
+        Console.WriteLine(res);
+        Console.ReadKey();
+
+    }
+}
