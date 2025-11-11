74b4e59fc4d45e5c5cd20ad0f5e7657ba5e3cbf9
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-11-11 22:59:58 +0500

    5

---
+++ Tyuiu.BayazitovEA.Sprint4.Task4.V12.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.BayazitovEA.Sprint4.Task4.V12.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.BayazitovEA.Sprint4.Task4.V12/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.BayazitovEA.Sprint4.Task4.V12.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        int[,] matrix = new int[5, 5];
+
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+
+                matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        int[,] result = ds.Calculate(matrix);
+
+        PrintMatrix(result);
+
+        Console.ReadKey();
+    }
+
+    static void PrintMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
