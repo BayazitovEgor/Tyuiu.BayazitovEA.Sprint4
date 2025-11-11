daf861ced23cfb4116dfe75797bf85deccc3bbf8
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-11-11 23:38:44 +0500

    7

---
+++ Tyuiu.BayazitovEA.Sprint4.Task6.V4.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.BayazitovEA.Sprint4.Task6.V4.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.BayazitovEA.Sprint4.Task6.V4/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BayazitovEA.Sprint4.Task6.V4.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        var week = new string[] { "Москва", "Санкт Петербург", "Новосибирск", "Екатеринбург", "Нижний Новгород", "Казахстан", "Самара" };
+        string[] nums = ds.Calculate(week);
+        foreach (string city in nums)
+        {
+            Console.WriteLine(city);
+        }
+
+        Console.ReadLine();
+    }
+
+}   
