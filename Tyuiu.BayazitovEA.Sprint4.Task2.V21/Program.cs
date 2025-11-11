a95d42fe91dc21735f32e61e672e607c1409b3d4
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-11-11 21:31:23 +0500

    3

---
+++ Tyuiu.BayazitovEA.Sprint4.Task2.V21/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BayazitovEA.Sprint4.Task2.V21.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random rnd= new Random();
+        DataService ds = new DataService();
+        int len = Convert.ToInt32(Console.ReadLine());
+        int[] numsArry = new int[len];
+        for (int i = 0; i <= len - 1; i++)
+        {
+            numsArry[i] = rnd.Next(5,20);
+        }
+        int res = ds.Calculate(numsArry);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
