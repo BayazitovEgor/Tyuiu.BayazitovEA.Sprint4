1f2ae5f5e0d755192baf67b26180b4b2029d770d
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-11-11 21:06:31 +0500

    2

---
+++ Tyuiu.BayazitovEA.Sprint4.Task1.V3.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.BayazitovEA.Sprint4.Task1.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.BayazitovEA.Sprint4.Task1.V3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Tyuiu.BayazitovEA.Sprint4.Task1.V3.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int len;
+        len = Convert.ToInt32(Console.ReadLine());
+        int[] numsArry=new int[len];
+        for (int i=0;i<=len-1;i++)
+        {
+            numsArry[i]=Convert.ToInt32(Console.ReadLine());
+        }
+        int res = ds.Calculate(numsArry);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
