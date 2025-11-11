130e050a244ee53de9f7e93ee68008f2b1361783
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-11-11 21:57:27 +0500

    4

---
+++ Tyuiu.BayazitovEA.Sprint4.Task3.V23.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.BayazitovEA.Sprint4.Task3.V23.Lib
+{
+    public class DataService : ISprint4Task3V23
+    {
+        public int Calculate(int[,] array)
+        {
+            int rows = 5;
+            int cols = 5;
+            int count = 0;
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    if (array[i, j]%2==0)
+                    {
+                        count++;
+                    }
+                } }
+            return count;
+        }
+    } }
---
+++ Tyuiu.BayazitovEA.Sprint4.Task3.V23/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.BayazitovEA.Sprint4.Task3.V23.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[,] array = new int[5, 5] {{ 7, 5, 5, 6, 7 },
+
+{ 8, 7, 8, 4, 3 },
+
+{ 5, 6, 7, 8, 3 },
+
+{ 4, 2, 3, 6, 4},
+
+{5, 2, 4, 2, 3,}};
+    DataService ds = new DataService();
+        int res = ds.Calculate(array);
+        Console.WriteLine(res);
+    }
+}
