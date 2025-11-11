d4aec846bff0e07863b97e8bd575667a2fc9e398
Егор Баязитов <Tyuiu.BayazitovEA@outlook.com>
2025-11-11 23:15:18 +0500

    6

---
+++ Tyuiu.BayazitovEA.Sprint4.Task5.V30.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.BayazitovEA.Sprint4.Task5.V30.Lib
+{
+    public class DataService : ISprint4Task5V30
+    {
+        public int Calculate(int[,] matrix)
+        {
+            {
+                int rows = 5;
+                int cols = 5;
+                int sum = 0;
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        if (matrix[i, j] > 0 )
+                        {
+                            sum += matrix[i, j];
+                        }
+                    }
+                }
+                return sum;
+            }
+        }
+    }
+}
---
+++ Tyuiu.BayazitovEA.Sprint4.Task5.V30/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.BayazitovEA.Sprint4.Task5.V30.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        DataService ds = new DataService();
+        int[,] numsArry = new int[5, 5];
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                numsArry[i, j] = rnd.Next(-2, 4);
+            }
+        }
+        int res = ds.Calculate(numsArry);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
